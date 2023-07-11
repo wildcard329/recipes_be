087e20806886380b039f44e9ddcdefddbb7bc900
wildcard329 <enkiduwildman@gmail.com>
2023-07-11 10:30:15 -0700

    [users-001] updated routes for getting users

---
+++ repository/userRepository.js
@@ -0,0 +1,22 @@
+const db = require('../config/pg');
+
+const getUsers = () => db.query('SELECT * FROM users ORDER BY user_id');
+
+const getUserById = (id) => db.query('SELECT * FROM users WHERE user_id = $1', [id]);
+
+const getUserByEmail = (email) => db.query('SELECT * FROM uswers WHERE email = $1', [email]);
+
+const addUser = ({ username, password, email, timestamp }) => db.query('INSERT INTO users (user_name, password, email, created_on) VALUES ($1, $2, $3, $4) RETURNING user_id', [username, password, email, timestamp]);
+
+const updateUser = ({ username, password, id }) => db.query('UPDATE users SET user_name = $1, password = $2 WHERE user_id = $3', [username, password, id])
+
+const removeUser = (id) => db.query('DELETE FROM users WHERE user_id = $1', [id]);
+
+module.exports = {
+  getUsers,
+  getUserById,
+  getUserByEmail,
+  addUser,
+  updateUser,
+  removeUser,
+};
