a63b839f21bd1356feeba016066e444363b8f308
wildcard329 <enkiduwildman@gmail.com>
2023-07-13 22:29:53 -0700

    [users-010] moved controller logic to services and created a controller for users

--- route/protected/userRouter.js
+++ route/protected/userRouter.js
@@ -4,12 +4,12 @@ const serverRes = require('../../util/middleware/res.js');
 
 router.get('/', async (req, res) => await serverRes.compileRes(res, users.getUsers))
 
-router.get('/:id', async (req, res) => await serverRes.compileRes(res, users.getUserById, parseInt(req.params.id)));
+router.get('/:id', async (req, res) => await serverRes.compileRes(res, users.getUserById, req.params.id));
 
 router.post('/', async (req, res) => await serverRes.compileRes(res, users.addUser, req.body));
 
 router.put('/', async (req, res) => await serverRes.compileRes(res, users.updateUser, req.body));
 
-router.delete('/:id', async (req, res) => await serverRes.compileRes(res, users.removeUser, parseInt(req.params.id)));
+router.delete('/:id', async (req, res) => await serverRes.compileRes(res, users.removeUser, req.params.id));
 
 module.exports = router;
