83dec4d723a3be0d251d7b3b7dd04da25b55c042
wildcard329 <enkiduwildman@gmail.com>
2023-07-17 20:38:25 -0700

    [rec-002] added routes for recipes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const { verifyToken } = require('../util/middleware/jwt.js');
 
 const users = require('../route/protected/userRouter.js');
 const auth = require('../route/public/authRoute.js');
+const publicRecipes = require('../route/public/recipesRouter.js');
+const privateRecipes = require('../route/protected/userRecipesRouter.js');
 
 const server = express();
 server.use(express.json());
@@ -14,5 +16,7 @@ server.use(cors());
 
 server.use('/api/users', verifyToken, users);
 server.use('/api/auth', auth);
+server.use('/api/recipes', publicRecipes);
+server.use('/api/private/recipes', verifyToken, privateRecipes);
 
 module.exports = server;
